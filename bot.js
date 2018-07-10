10009d123c6a9905d815409e06def450c953926a
Sattam-44 <41042065+Sattam-44@users.noreply.github.com>
2018-07-10 10:26:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 const request = require('request');
 const fs = require('fs');
@@ -315,7 +284,3 @@ For additional help,  `)
          message.channel.send({embed:embed});
                         }
                     });
-
-
-
-client.login(process.env.BOT_TOKEN);
