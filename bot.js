cfecfd7c1347a466c0b86677340a45ccd52d24bd
Sattam-44 <41042065+Sattam-44@users.noreply.github.com>
2018-08-19 15:58:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,18 +52,9 @@ client.on("message", message => {
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
-};     
+};
+	
 });
-
-
-
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
 
 
 
