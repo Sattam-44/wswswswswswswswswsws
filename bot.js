aa4edc2a4773da78f4e940af3660ba810ce1d162
Sattam-44 <41042065+Sattam-44@users.noreply.github.com>
2018-08-21 04:50:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`RENAD`,"http://twitch.tv/S-F")
+client.user.setGame(`Sattam Test`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
