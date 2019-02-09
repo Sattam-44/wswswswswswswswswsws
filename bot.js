69b2927472a960eb98293fb4c7c676df44f72cdf
Sattam-44 <41042065+Sattam-44@users.noreply.github.com>
2019-02-09 23:54:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,13 +35,6 @@ client.user.setGame(`i Love you ❤ `,"http://twitch.tv/S-F")
 
 
 
-client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('449997332952383512');
-  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
-  const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
-  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
-});
 
 
 client.login(process.env.BOT_TOKEN);
